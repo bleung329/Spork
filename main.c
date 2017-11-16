db28f207a46dc84554a9f73482aad63320ba7711
ykadir42 <ykadir42@gmail.com>
2017-11-16 15:39:08 -0500

    fork off children

--- main.c
+++ main.c
@@ -1,4 +1,30 @@
-int main{
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
 
+int main{
+  
+  srand(time(NULL));
+
+  int f = fork();
+
+  printf("some initial message");
+
+  if(f){
+    printf("pid: %d", getpid());
+    sleep(rand() % 15 + 5);
+    printf("a message that it is finished");
+  }
+  else{
+    if(f){
+      printf("pid: %d", getpid());
+      sleep(rand() % 15 + 5);
+      printf("a message that it is finished");
+    }
+    else{
+      int signal = 2;
+      wait(&signal);
+    }
+  }
   return 0;
 }
